68fe01187d39bf648b47a64a838d041f665ad910
xrotwang <xrotwang@googlemail.com>
2020-09-15 11:26:05 +0200

    initial import and skeleton

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import json
+
+
+with open('metadata.json', encoding='utf-8') as fp:
+    metadata = json.load(fp)
+
+
+setup(
+    name='lexibank_spagnolmaltese',
+    description=metadata['title'],
+    license=metadata.get('license', ''),
+    url=metadata.get('url', ''),
+    py_modules=['lexibank_spagnolmaltese'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'lexibank.dataset': [
+            'spagnolmaltese=lexibank_spagnolmaltese:Dataset',
+        ]
+    },
+    install_requires=[
+        'pylexibank>=2.7.1',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
